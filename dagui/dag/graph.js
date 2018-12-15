5af7893510d27e21e10617813f988818d902171d
aatack <alexanderjtatack@gmail.com>
2018-12-15 19:29:22 +0000

    Add function for adding subgraphs

--- dagui/dag/graph.js
+++ dagui/dag/graph.js
@@ -13,16 +13,58 @@ class Graph {
         }
         graph[name] = node;
 
-        var notifyInputs = function(input) {
+        Graph.notifyInputs(node, node.inputs);
+    }
+
+    /**
+     * Notify each of the inputs of a Node that they
+     * are outputting their value to that node if they are part
+     * of the specified graph.
+     * @param {Node} node 
+     */
+    static notifyInputs(node) {
+        var notifyInputsInner = function(input) {
             if (input instanceof Array) {
-                input.forEach(i => notifyInputs(i));
+                input.forEach(i => notifyInputsInner(i));
             } else if (input instanceof Node) {
-                input.dependents.push(node);
+                if (input.dependents.indexOf(node) === -1) {
+                    input.dependents.push(node);
+                }
             } else {
-                forObject(notifyInputs, input)
+                forObject(notifyInputsInner, input)
             }
         }
+        notifyInputsInner(node.inputs);
+    }
 
-        notifyInputs(node.inputs);
+    /**
+     * Add a subgraph to the given graph, adding each of the subgraph's
+     * nodes to the list of descendents of each of their inputs.
+     * @param {object} graph 
+     * @param {string} name 
+     * @param {object} subgraph 
+     */
+    static addGraph(graph, name, subgraph) {
+        if (graph[name] !== undefined) {
+            console.error("A subgraph named " + name + " already exists.");
+            return;
+        }
+        graph[name] = subgraph;
+        Graph.iterateNodes(n => Graph.notifyInputs(n), subgraph);
+    }
+
+    /**
+     * Execute a function on each Node of the given graph.
+     * @param {function} f 
+     * @param {object} graph 
+     */
+    static iterateNodes(f, graph) {
+        for (var property in graph) {
+            if (graph[property] instanceof Node) {
+                f(graph[property]);
+            } else {
+                Graph.iterateNodes(f, graph[property]);
+            }
+        }
     }
 }
