963c1c75bb48a7cf0e7a7f2cc59f78fdcbe7aef9
aatack <alexanderjtatack@gmail.com>
2018-12-18 00:15:59 +0000

    Rework Node class to have abstract applyDeltas only

--- dagui/dag/node.js
+++ dagui/dag/node.js
@@ -1,41 +1,19 @@
 class Node {
     /**
-     * Create a Node object.  If applyDelta and/or applyDeltas are not provided,
-     * default implementations will be created.
-     * 
-     * recalculate: given a value, dictionary, or list - created by taking the values
-     * of each of the nodes in the input object - calculate the value of this node
-     * and return it.
-     * 
-     * applyDelta: given a node and one delta object, apply the delta to the node
-     * and return a new delta describing changes to the node, if any.
-     * 
-     * applyDeltas: given a node, apply all of the queued deltas to the object,
-     * updating its value if needed, clearing all queued deltas, and noting that
-     * the node's value is accurate.  This should return a list of deltas desribing
-     * the changes made to the node's value, if any.
-     * 
-     * All of the above values may assume that the node's inputs' values are
-     * up to date.
-     * 
+     * Create a Node object, whose value is a function of the its dependent
+     * nodes.  The inputs should be either a Node, an object whose leaves
+     * are Node objects, or a list of Node objects.  The value of the node
+     * is calculated by calling applyDeltas() whenever its value is requested. 
      * @param {object} inputs 
-     * @param {function} recalculate 
-     * @param {function} applyDelta 
-     * @param {function} applyDeltas 
      * @param {[function]} events
      */
-    constructor(inputs, recalculate, applyDelta = null, applyDeltas = null,
-            events = []) {
+    constructor(inputs, events = []) {
         this.value = null;
         this.valueAccurate = false;
 
         this.inputs = inputs;
         this.dependents = [];
         this.graphStack = [];
-
-        this.recalculate = recalculate;
-        this.applyDelta = applyDelta;
-        this.applyDeltas = applyDeltas !== null ? applyDeltas : this.defaultApplyDeltas();
 
         this.events = events;
 
@@ -45,10 +23,13 @@ class Node {
     /**
      * Applies all pending deltas.  This is used instead of simply
      * calling applyDeltas publicly so that events and responses to
-     * being updated can be added later on.
+     * being updated can be added later on.  This will also set
+     * valueAccurate to true and clear any queued deltas.
      */
     update() {
-        var deltas = this.applyDeltas(this);
+        var deltas = this.applyDeltas();
+        this.valueAccurate = true;
+        this.queuedDeltas = [];
         this.events.forEach(e => e(deltas));
         return deltas;
     }
@@ -78,115 +59,13 @@ class Node {
     }
 
     /**
-     * Return a default implementation of applyDeltas from the definitions
-     * of either applyDelta or recalculate.
+     * Update the value of the node according to all the deltas
+     * which have been queued for it.  This function is not required
+     * to clear the queued deltas or to set valueAccurate to true.
+     * It may be assumed that all inputs are up to date when this
+     * function is called.
      */
-    defaultApplyDeltas() {
-        if (this.applyDelta === null) {
-            return function(self) {
-                self.value = self.recalculate(self.getInputValues());
-                self.queuedDeltas = [];
-                self.valueAccurate = true;
-                return ["recalculated"];
-            }
-        } else {
-            return function(self) {
-                var newDeltas = [];
-                for (var i = 0; i < self.queuedDeltas.length; i++) {
-                    newDeltas.push(self.applyDelta(self, self.queuedDeltas[i]));
-                }
-                self.queuedDeltas = [];
-                self.valueAccurate = true;
-                return newDeltas;
-            }
-        }
+    applyDeltas() {
+        console.error("Node.applyDeltas not implemented.");
     }
-}
-
-/**
- * Create a node whose value cannot be changed over time.
- * @param {value} value 
- */
-function constantNode(value) {
-    var n = new Node([], _ => value, null, function(node) {
-        node.value = value;
-        node.valueAccurate = true;
-        if (node.queuedDeltas.length > 0) {
-            console.error("Cannot apply deltas to a constant node.");
-            node.queuedDeltas = [];
-        }
-        return [];
-    }, events = []);
-    n.value = value;
-    n.valueAccurate = true;
-    return n;
-}
-
-/**
- * Create a node whose value can be changed by queueing a delta
- * which equals that value.
- * @param {value} defaultValue 
- */
-function variableNode(defaultValue = null) {
-    var n = new Node([], _ => defaultValue, null, function(node) {
-        if (node.queuedDeltas.length === 0) {
-            return [];
-        } else {
-            var newValue = node.queuedDeltas[node.queuedDeltas.length - 1];
-            var oldValue = node.value;
-            node.value = newValue;
-            node.valueAccurate = true;
-            node.recalculate = _ => newValue;
-            node.queuedDeltas = [];
-            return [{ oldValue: oldValue, newValue: newValue }];
-        }
-    }, events = []);
-    n.value = defaultValue;
-    n.valueAccurate = true;
-    return n;
-}
-
-/**
- * Return a node which applies a monadic function to the value
- * of another node.
- * @param {function} f 
- * @param {Node} argumentNode 
- */
-function monadicNode(f, argumentNode) {
-    n = new Node(argumentNode, x => f(x), null, function(node) {
-        if (node.queuedDeltas.length > 0) {
-            node.queuedDeltas = [];
-            node.valueAccurate = true;
-            var oldValue = node.value;
-            var newValue = node.recalculate(node.getInputValues());
-            if (newValue != oldValue) {
-                node.value = newValue;
-                return [{ newValue: newValue, oldValue: oldValue }];
-            } else {
-                return [];
-            }
-        }
-    }, events = []);
-    n.queuedDeltas.push("initialise");
-    return n;
-}
-
-/**
- * Return a node which applies a diadic function to the values
- * of two other nodes.
- * @param {function} f 
- * @param {string} firstArgumentName 
- * @param {string} secondArgumentName 
- * @param {Node} firstArgumentNode 
- * @param {Node} secondArgumentNode 
- */
-function diadicNode(f, firstArgumentName, secondArgumentName,
-        firstArgumentNode, secondArgumentNode) {
-    var inputValues = {};
-    inputValues[firstArgumentName] = firstArgumentNode;
-    inputValues[secondArgumentName] = secondArgumentNode;
-    n = new Node(inputValues,
-        d => f(d[firstArgumentName], d[secondArgumentName]));
-    n.queuedDeltas.push("initialise");
-    return n;
 }
