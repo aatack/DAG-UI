e8a9b69e9a56da47db67ff5c0b3def2fee2515f3
aatack <alexanderjtatack@gmail.com>
2019-04-19 23:46:22 +0100

    Refine Delta interface

--- dag/delta.js
+++ dag/delta.js
@@ -2,12 +2,10 @@ class Delta {
 
     /**
      * Create a new Delta of a specific type.
-     * @param {Unit} source 
      * @param {string} type
      * @param {Delta -> Delta} cast
      */
     constructor(source, type, cast) {
-        this.source = source;
         this.type = type;
         this.cast = cast;
     }
@@ -26,41 +24,24 @@ class Delta {
 
     /**
      * A Delta denoting that a Unit's value has been altered.
-     * @param {Unit} unit 
+     * @param {any} newValue 
      */
-    static changed(unit) {
-        var delta = new Delta(unit, "changed", function (d) {
+    static changed(newValue) {
+        var delta = new Delta("changed", function (d) {
             throw "cannot cast a changed Delta";
         });
-        delta.newValue = unit.value;
+        delta.newValue = newValue;
         return delta;
     }
 
     /**
      * A Delta denoting that, while a Unit's value was updated, it
      * remained unchanged.
-     * @param {Unit} unit 
      */
-    static unchanged(unit) {
-        return new Delta(unit, "unchanged", function (d) {
+    static unchanged() {
+        return new Delta("unchanged", function (d) {
             throw "cannot cast an unchanged Delta";
         });
-    }
-
-    static incremented(unit, by = 1) {
-        var delta = new Delta(unit, "incremented", function (d) {
-            return Delta.changed(unit);
-        });
-        delta.by = by;
-        return delta;
-    }
-
-    static decremented(unit, by = 1) {
-        var delta = new Delta(unit, "decremented", function (d) {
-            return Delta.incremented(unit, by = -by);
-        })
-        delta.by = by;
-        return delta;
     }
 
 }
