01828983367e3622e3f64fb6abb7e35413c4a7fd
aatack <alexanderjtatack@gmail.com>
2019-04-20 21:59:19 +0100

    Define newer simplified unit class

---
+++ dagui/init.js
@@ -0,0 +1,7 @@
+dagui = {
+    int: i => new Int(i),
+    float: x => new Float(x),
+    copy: u => new Copy(u),
+};
+
+dag = dagui;
---
+++ dagui/unit.js
@@ -0,0 +1,67 @@
+class Unit {
+
+    /**
+     * Create a new unit, giving a list of those units from which it takes inputs.
+     * @param {Object} inputs
+     */
+    constructor(inputs) {
+        this.inputs = inputs;
+        for (var key in inputs) {
+            inputs[key].addDependent(this);
+        }
+        this.dependents = [];
+        this.active = true;
+        this.updateCallbacks = [];
+
+        this.value = null;
+        this.recalculateValue();
+    }
+
+    /**
+     * Recalculate the value of the node, assuming at least one of its input
+     * units has had its value change.
+     */
+    recalculateValue() {
+        throw "Unit.recalculateValue not implemented";
+    }
+
+    /**
+     * Register that another unit depends on this one.
+     * @param {Unit} dependent 
+     */
+    addDependent(dependent) {
+        this.dependents.push(dependent);
+    }
+
+    /**
+     * Update the value of the unit, calling its update callbacks if it is active.
+     */
+    update() {
+        if (this.active) {
+            this.recalculateValue();
+            for (var i in this.updateCallbacks) {
+                this.updateCallbacks[i](this);
+            }
+            for (var i in this.dependents) {
+                this.dependents[i].update();
+            }
+        }
+    }
+
+    /**
+     * Deactivate the unit, so its values will not be activated automatically.
+     */
+    deactivate() {
+        this.active = false;
+    }
+
+    /**
+     * Activate the unit, denoting that its value should be updated instantly
+     * in the future.
+     */
+    activate() {
+        this.active = true;
+        this.update();
+    }
+
+}
