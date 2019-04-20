0e6073826f6611af1fb15626286938b083e8cdc3
aatack <alexanderjtatack@gmail.com>
2019-04-20 23:38:57 +0100

    Fix onload and add box element

---
+++ dagui/elements.js
@@ -0,0 +1,24 @@
+/**
+ * Create an empty box with the given parameters and return an object
+ * with each of its properties as well as an HTML element.
+ * @param {numeric} top 
+ * @param {numeric} left 
+ * @param {numeric} height 
+ * @param {numeric} width 
+ */
+function box(top, left, height, width) {
+    var output = {
+        top: dag.wrap(top),
+        left: dag.wrap(left),
+        height: dag.wrap(height),
+        width: dag.wrap(width)
+    }
+    output.element = document.createElement("div");
+    output.element.style["position"] = "fixed";
+    ["top", "left", "height", "width"].forEach(function (a) {
+        output[a].tie(output.element, "/" + a);
+        output[a].update();
+    });
+    dag.frame.appendChild(output.element);
+    return output;
+}
--- dagui/init.js
+++ dagui/init.js
@@ -1,10 +1,14 @@
-dagui = {
+dag = {
     int: i => new Int(i),
     float: x => new Float(x),
     boolean: b => new Boolean(b),
     string: s => new String(s),
     copy: u => new Copy(u),
     wrap: v => wrap(v),
+
+    "box": box,
 };
 
-dag = dagui;
+window.onload = function () {
+    dag.frame = document.getElementById("dagframe");
+}
