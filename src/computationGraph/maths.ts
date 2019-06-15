8e19bd94aa7adf452ed6e6289fc5903f7d7518bd
aatack <alexanderjtatack@gmail.com>
2019-06-15 15:59:25 +0100

    Lay out structure of computation graph classes

---
+++ src/computationGraph/maths.ts
@@ -0,0 +1,19 @@
+class Sum extends Template {
+
+}
+
+class Difference extends Template {
+
+}
+
+class Product extends Template {
+
+}
+
+class Ratio extends Template {
+
+}
+
+class Square extends Template {
+
+}
---
+++ src/computationGraph/node.ts
@@ -0,0 +1,19 @@
+class Int extends Template {
+
+}
+
+class Float extends Template {
+
+}
+
+class Bool extends Template {
+
+}
+
+class Str extends Template {
+
+}
+
+class List extends Template {
+
+}
