b313f369ae17e8767b9ea112234e3d6209d8934c
aatack <alexanderjtatack@gmail.com>
2019-06-15 19:21:34 +0100

    Define Template interface

--- src/computationGraph/template.ts
+++ src/computationGraph/template.ts
@@ -1,3 +1,23 @@
-class Template {
+interface Template {
+
+    inputs: [string, Template][];
+    outputs: [string, Template][];
+    hidden: [string, Template][];
+
+    /*
+    Put the objects in the template into a JSON object.
+    */
+    toJson(unwrapValues: boolean): object;
+
+    /*
+    Overwrite the current sub-template values using those from a
+    JSON object.
+    */
+    fromJson(json: object): void;
+
+    /*
+    Calculate the values of all outputs in-place from the inputs.
+    */
+    recalculate(): void;
 
 }
