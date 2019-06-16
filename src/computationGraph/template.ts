904d0086386145d992b345317f1056103fb19ba5
aatack <alexanderjtatack@gmail.com>
2019-06-17 00:57:10 +0100

    Convert Template to an abstract class

--- src/computationGraph/template.ts
+++ src/computationGraph/template.ts
@@ -1,4 +1,4 @@
-interface Template {
+abstract class Template {
 
     inputs: [string, Template][];
     outputs: [string, Template][];
@@ -6,17 +6,17 @@ interface Template {
     /*
     Put the objects in the template into a JSON object.
     */
-    toJson(unwrapValues: boolean): object;
+    abstract toJson(unwrapValues: boolean): object;
 
     /*
     Overwrite the current sub-template values using those from a
     JSON object.
     */
-    fromJson(json: object): void;
+    abstract fromJson(json: object): void;
 
     /*
     Calculate the values of all outputs in-place from the inputs.
     */
-    recalculate(): void;
+    abstract recalculate(): void;
 
 }
