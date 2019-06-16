2d1833d77f29beb7849651d2fc372e398899aaf9
aatack <alexanderjtatack@gmail.com>
2019-06-17 00:37:58 +0100

    Get rid of individual value types

--- src/computationGraph/node.ts
+++ src/computationGraph/node.ts
@@ -9,98 +9,43 @@ class Value implements Template {
      * Create a new Value.
      */
     constructor(value: any) {
-        this.value = this.cast(value);
+        this.value = value;
         this.inputs = [];
         this.outputs = [];
     }
 
-    /*
-    Check the value's type and cast it if necessary.
-    */
-    cast(value: any): any {
-        return value;
-    }
-
-    /*
-    Put the objects in the template into a JSON object.
-    */
+    /**
+     * Put the objects in the template into a JSON object.
+     */
     toJson(unwrapValues: boolean): object {
-        return unwrapValues ? this : this.value;
+        return unwrapValues ? this : this.get();
     }
 
-    /*
-    Overwrite the current sub-template values using those from a
-    JSON object.
-    */
+    /**
+     * Overwrite the current sub-template values using those from a
+     * JSON object.
+     */
     fromJson(json: object): void {
-        this.value = this.cast(json);
+        this.set(json);
     }
 
-    /*
-    Calculate the values of all outputs in-place from the inputs.
-    */
+    /**
+     * Calculate the values of all outputs in-place from the inputs.
+     */
     recalculate(): void { }
 
-}
-
-class Int extends Value {
-
-    /*
-    Check the value's type and cast it if necessary.
-    */
-    cast(value: any): any {
-        return <number>value;
+    /**
+     * Set the node to a particular value.
+     */
+    set(value: any): void {
+        this.value = value;
     }
 
-}
-
-class Float extends Value {
-
-    /*
-    Check the value's type and cast it if necessary.
-    */
-    cast(value: any): any {
-        return <number>value;
-    }
-
-}
-
-class Bool extends Value {
-
-    /*
-    Check the value's type and cast it if necessary.
-    */
-    cast(value: any): any {
-        return <boolean>value;
-    }
-
-}
-
-class Str extends Value {
-
-    /*
-    Check the value's type and cast it if necessary.
-    */
-    cast(value: any): any {
-        return <string>value;
-    }
-
-}
-
-class List extends Value {
-
-    /*
-    Check the value's type and cast it if necessary.
-    */
-    cast(value: any): any {
-        return value.map(this.innerCast);
-    }
-
-    /*
-    Cast a value within the list to the correct type.
-    */
-    innerCast(value: any): any {
-        return value;
+    /**
+     * Get the node's value, unwrapped.
+     */
+    get(): any {
+        return this.value;
     }
 
 }
