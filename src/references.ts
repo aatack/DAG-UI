4cf36bdb5a80dfe892df3df2aa8b870678413a89
aatack <alexanderjtatack@gmail.com>
2019-06-22 21:33:45 +0100

    Add references file

---
+++ src/references.ts
@@ -0,0 +1,4 @@
+/// <reference path="computation/linked.ts"/>
+/// <reference path="computation/maths.ts"/>
+/// <reference path="computation/template.ts"/>
+/// <reference path="computation/value.ts"/>
