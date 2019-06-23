f901f46458e693d0a620cc1f8a2507a424638b33
aatack <alexanderjtatack@gmail.com>
2019-06-23 14:49:33 +0100

    Remove old references file

--- src/references.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="computation/linked.ts"/>
-/// <reference path="computation/maths.ts"/>
-/// <reference path="computation/template.ts"/>
-/// <reference path="computation/value.ts"/>
