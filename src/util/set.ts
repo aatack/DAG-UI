c08b7140ebf090588cfb54e74b184206b8a4408c
aatack <alexanderjtatack@gmail.com>
2019-06-30 18:50:13 +0100

    Change SetFunctions from static class to namespace

--- src/util/set.ts
+++ src/util/set.ts
@@ -1,10 +1,10 @@
-export class SetFunctions {
+export namespace SetFunctions {
 
     /**
      * Find the set of objects which are contained in at least
      * one of the input sets.
      */
-    static union<T>(a: Set<T>, b: Set<T>): Set<T> {
+    export function union<T>(a: Set<T>, b: Set<T>): Set<T> {
         var c = new Set<T>();
         a.forEach(o => c.add(o));
         b.forEach(o => c.add(o));
@@ -15,7 +15,7 @@ export class SetFunctions {
      * Find the set of objects which are contained in both of
      * the two input sets.
      */
-    static intersection<T>(a: Set<T>, b: Set<T>): Set<T> {
+    export function intersection<T>(a: Set<T>, b: Set<T>): Set<T> {
         var c = new Set<T>();
         a.forEach(o => { if (b.has(o)) { c.add(o) } });
         return c;
@@ -25,7 +25,7 @@ export class SetFunctions {
      * Find the set of objects which are in the first input set
      * but not the second input set.
      */
-    static difference<T>(a: Set<T>, b: Set<T>): Set<T> {
+    export function difference<T>(a: Set<T>, b: Set<T>): Set<T> {
         var c = new Set(a);
         b.forEach(c.delete);
         return c;
