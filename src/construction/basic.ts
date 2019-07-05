8e1ff1cd949ad035ea02e5facdaae3a0ba218f42
aatack <alexanderjtatack@gmail.com>
2019-07-05 20:43:52 +0100

    Implement abstract basic relation

---
+++ src/construction/basic.ts
@@ -0,0 +1,38 @@
+import { Relation } from "./relation";
+import { Pointer } from "../util/pointer";
+import { Template } from "../computation/template";
+
+export abstract class BasicRelation extends Relation {
+
+    possibleTemplates: Template[];
+
+    /**
+     * Defines a relation which determines an application order by brute
+     * force, cycling through a list of possible templates until one is
+     * found which can be applied to a set of known types.
+     */
+    constructor(possibleTemplates: Template[]) {
+        super();
+        this.possibleTemplates = possibleTemplates;
+    }
+
+    /**
+     * Determine whether the relation can be used to resolve the types
+     * of any unknown values in the graph.  If it can, modify their
+     * type with the missing value and return a template or list of
+     * templates which would need to be applied to compute that change
+     * on an object of live values.
+     */
+    resolve(knownTypes: any): Template[] {
+        var applicableTemplates: Template[] = [];
+
+        this.possibleTemplates.forEach(
+            template => this.checkTemplate(
+                template, knownTypes, applicableTemplates
+            )
+        );
+
+        return applicableTemplates;
+    }
+
+}
