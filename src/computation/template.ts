bac3a80bf8f505ee5c3df953230d5841dd38c384
aatack <alexanderjtatack@gmail.com>
2019-07-09 19:11:22 +0100

    (Unstable) make revisions to the template class

--- src/computation/template.ts
+++ src/computation/template.ts
@@ -1,87 +1,74 @@
 import { Pointer } from "../util/pointer";
-import { SetFunctions } from "../util/set";
+import { Structure } from "../util/structure";
+import { Kind } from "../typing/kind";
 
 export abstract class Template {
 
-    inputs: Set<string>;
-    outputs: Set<string>;
+    inputs: Structure<string>;
+    outputs: Structure<string>;
 
-    private inputLookup: { [index: string]: Pointer } = {};
-    private outputLookup: { [index: string]: Pointer } = {};
+    private inputPointers: Structure<Pointer>;
+    private outputPointers: Structure<Pointer>;
 
     /**
      * Create a new template, which codifies a computation which can be
      * performed on a JavaScript object.
      */
-    constructor(inputs: Set<string>, outputs: Set<string>) {
+    constructor(inputs: Structure<string>, outputs: Structure<string>) {
         this.inputs = inputs;
         this.outputs = outputs;
 
-        this.inputs.forEach(s => this.inputLookup[s] = Pointer.wrap(s));
-        this.outputs.forEach(s => this.outputLookup[s] = Pointer.wrap(s));
-
-        this.checkCyclicDependencies();
+        this.inputPointers = this.inputs.map<string, Pointer>(s => Pointer.wrap(s));
+        this.outputPointers = this.outputs.map<string, Pointer>(s => Pointer.wrap(s));
     }
 
     /**
-     * Throw an error if there are any cyclic dependencies.
+     * Given a structure of input values which have been lifted from
+     * the relevant source object, compute the output values that would
+     * result from computing them.  If an output value cannot be calculated
+     * from the given inputs, it should be set to undefined.  The returned
+     * structure must have the same layout as the template's output structure.
      */
-    private checkCyclicDependencies(): void {
-        var all = SetFunctions.union(this.inputs, this.outputs);
-        if (all.size < this.inputs.size + this.outputs.size) {
-            throw new Error(
-                "cyclic dependency found: a value is pointed " +
-                "to in both the inputs and outputs of a template"
-            );
-        }
+    abstract compute(resolvedInputs: Structure<any>): Structure<any>;
+
+    /**
+     * Given a structure of kinds representing each input, some of which may be
+     * unknown, return a structure whose layout is the same as this template's
+     * output structure but whose values are the kinds that would be produced if
+     * this template were applied to an input structure of those types.
+     */
+    abstract determineSchema(inputKinds: Structure<Kind>): Structure<Kind>;
+
+    /**
+     * Taking input values from a given source object, compute the values of
+     * any outputs where possible, and then apply those outputs to a target
+     * object.
+     */
+    apply(source: any, target: any): void {
+        var alterations = this.compute(this.resolveInputs(source));
+        Structure.zip(this.outputPointers, alterations).forEach(pair => {
+            var [pointer, value] = pair;
+            if (value !== undefined) {
+                pointer.set(target, value);
+            }
+        });
     }
 
     /**
-     * Apply the template to a given target, modifying it in place.
-     * Return a set of pointers to those outputs which were changed
-     * by applying the operation.
+     * Lift the values of each of the template's inputs from the
+     * given source object.  If a value does not exist in the source, it
+     * will be resolved to undefined.
      */
-    abstract apply(target: any, changedInputs: Set<string>): Set<string>;
-
-    /**
-     * Given an object whose structure models that of the real input structure,
-     * but whose roots contain type variable instead of actual values, return
-     * a dictionary mapping the paths of this template's output nodes to the
-     * types they would take on if the template were applied to an input object
-     * whose values had those types.  A type of undefined means that the type
-     * is not specified or otherwise unavailable (eg. the input types would
-     * cause an error.)
-     */
-    abstract determineSchema(
-        inputTypes: { [index: string]: any }
-    ): { [index: string]: any };
+    private resolveInputs(source: any): Structure<any> {
+        return this.inputPointers.map<Pointer, any>(p => p.get(source));
+    }
 
     /**
      * Given a schema object, for each input extract the type of that input.
      * A value of undefined means the type of that value is not yet known.
      */
-    getInputTypes(source: any): { [index: string]: any } {
-        var output: { [index: string]: any } = {};
-        this.inputs.forEach(p => {
-            output[p] = this.getInput(p).get(source);
-        });
-        return output;
-    }
-
-    /**
-     * Get a pointer object from a path, assuming it points to one
-     * of the template's inputs.
-     */
-    getInput(path: string): Pointer {
-        return this.inputLookup[path];
-    }
-
-    /**
-     * Get a pointer object from a path, assuming it points to one
-     * of the template's outputs.
-     */
-    getOutput(path: string): Pointer {
-        return this.outputLookup[path];
+    getInputKinds(source: Structure<Kind>): Structure<Kind> {
+        return this.inputPointers.map<Pointer, Kind>(p => p.get(source));
     }
 
 }
