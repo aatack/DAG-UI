b8b5e3e66a65fba2865cd5559df6d5e9a50742f0
aatack <alexanderjtatack@gmail.com>
2019-07-18 00:06:39 +0100

    Add util functions to kind structures

---
+++ src/typing/utils.ts
@@ -0,0 +1,40 @@
+import { Kind, Kinds } from "./kind";
+import { Pointer } from "../util/pointer";
+import { Structure } from "../util/structure";
+
+export namespace KindUtils {
+
+    /**
+     * Display the kind structure in a human-legible format.
+     */
+    export function print(kinds: Structure<Kind>): void {
+        console.log(kinds.map(k => k.display()).unwrap());
+    }
+
+    /**
+     * Get a kind from a structure of kinds.  If the given kind does not exist,
+     * return the unknown kind.
+     */
+    export function getKindOrUnknown(kinds: Structure<Kind>, pointer: Pointer): Kind {
+        if (kinds.hasIndex(pointer.path)) {
+            return pointer.get(kinds);
+        } else {
+            return Kinds.unknown;
+        }
+    }
+
+    /**
+     * Dereference a pointer and set its value within a kinds dictionary.
+     * Specifically, if the value to which to set the kind is unknown while the
+     * value currently in the structure is not unknown, it will not be overridden.
+     * If the pointer is not currently contained in the structure, it will be created.
+     */
+    export function setKindIfNotUnknown(
+        kinds: Structure<Kind>, pointer: Pointer, value: Kind
+    ): void {
+        if (value != Kinds.unknown) {
+            pointer.set(kinds, value);
+        }
+    }
+
+}
