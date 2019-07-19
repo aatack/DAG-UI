8b634a849c3d1e8c8ef57517bb3412ec4fe8e014
aatack <alexanderjtatack@gmail.com>
2019-07-20 00:02:58 +0100

    Implement left to right scan

--- src/computation/collection.ts
+++ src/computation/collection.ts
@@ -60,3 +60,83 @@ export class Map extends Template {
     }
 
 }
+
+export class ScanLeftRight extends Template {
+
+    innerTemplate: Template;
+
+    initialiserPointer: Pointer;
+    sourcePointer: Pointer;
+    targetPointer: Pointer;
+
+    schemaSourcePointer: Pointer;
+    schemaTargetPointer: Pointer;
+
+    /**
+     * Create a template which scans from left to right over its source, assumed
+     * to be an ordered structure, and accumulates a value over each one, depositing
+     * that value in the corresponding cell of its target.  The values are
+     * calculated by a template, which is applied to a structure whose keys are
+     * `accumulated` for the accumulated value and `current` for the next item
+     * in the list.
+     */
+    constructor(
+        innerTemplate: Template,
+        initialiser: string,
+        source: string,
+        target: string
+    ) {
+        super(Structure.wrap<any>(source), Structure.wrap<any>(target));
+        this.innerTemplate = innerTemplate;
+
+        this.initialiserPointer = Pointer.wrap(initialiser);
+
+        this.sourcePointer = this.inputPointers.extractUnit();
+        this.schemaSourcePointer = this.sourcePointer.extendDownwards(["0"]);
+
+        this.targetPointer = this.outputPointers.extractUnit();
+        this.schemaTargetPointer = this.targetPointer.extendDownwards(["0"]);
+    }
+
+    /**
+     * Taking input values from a given source object, compute the values of
+     * any outputs where possible, and then apply those outputs to a target
+     * object.
+     */
+    apply(source: Structure<any>, target: Structure<any>): void {
+        var orderedSource = (
+            this.sourcePointer.get(source, false).extractOrdered()
+        );
+        var ordered = [this.initialiserPointer.get(source, false)];
+
+        for (var i = 0; i < orderedSource.length; i++) {
+            var innerTarget = Structure.empty();
+            this.innerTemplate.apply(
+                Structure.wrap({
+                    "accumulated": ordered[i],
+                    "current": orderedSource[i]
+                }),
+                innerTarget
+            );
+            ordered[i + 1] = innerTarget;
+        }
+        this.targetPointer.set(target, Structure.wrap(ordered));
+    }
+
+    /**
+     * Given a structure of kinds representing each input, some of which may be
+     * unknown, calculate any known kinds and place them in the target structure.
+     */
+    applySchema(sourceKinds: Structure<Kind>, targetKinds: Structure<Kind>): void {
+        var targetKind = Structure.empty<Kind>();
+        this.innerTemplate.applySchema(
+            Structure.wrap<Kind>({
+                "accumulated": this.initialiserPointer.get(sourceKinds, false),
+                "current": this.schemaSourcePointer.get(sourceKinds, false)
+            }),
+            targetKind
+        );
+        this.schemaTargetPointer.set(targetKinds, targetKind);
+    }
+
+}
